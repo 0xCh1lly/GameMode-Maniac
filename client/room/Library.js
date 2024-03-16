a4fc3f3d14f1be45ce7c6c8b2363a753e8871ca0
AlexandroRobert <162671978+AlexandroRobert@users.noreply.github.com>
2024-03-16 14:42:51 +0700

    Update Library.js

--- client/room/Library.js
+++ client/room/Library.js
@@ -2,14 +2,19 @@ import { DisplayValueHeader } from 'pixel_combats/basic';
 import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, BreackGraph, Ui, Properties, GameMode, Spawns, Timers, TeamsBalancer } from 'pixel_combats/room';
 
 
-function RandomMurder(player) {
+function RandomMurder() {
     const Index = Math.floor(Math.random() * Players.Count);
     Players.All[Index].Properties.Get("Murder").Value = true;
 }
 
-function RandomSheriff(player) {
-    const Index = Math.floor(Math.random() * Players.Count);
-    Players.All[Index].Properties.Get("Sheriff").Value = true;
-    Inventory.Secoundary.Value = true;
-    player.Damage.Damage.Value = true;
+function RandomSheriff() {
+    const Index = Math.floor(Math.random() * Players.Cotrue;
+    const Player = Players.All[Index];
+
+    if (Player.Properties.Get('Murder').Value) return RandomSheriff();
+    
+    Player.Properties.Get("Sheriff").Value = true;
+    Player.Inventory.Secondary.Value = true;
+    Player.Damage.FriendlyFire.Value = true;
+    Player.Damage.Damage.Value = true;
 }
