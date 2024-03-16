e9cc10efc2b2a31b966d2a783f57ee9fdb53dc4c
Ch1lly <132922392+0xCh1lly@users.noreply.github.com>
2024-03-16 13:53:56 +0500

    Обновление главного файла режима.

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -5,10 +5,63 @@ import * from "./Libray.js";
 
 
 const LeaderBoard_Time = 10;
+const MaxRounds_Count = 6;
 const PersonalTime = 30;
 const StartTime = 10;
 const SleepTime = 60;
 const EndTime = 10;
+
+const StateProp = Properties.GetContext().Get("StateProp");
+const RoundProp = Properties.GetContext().Get("RoundProp");
+const MainTimer = Timers.GetContext().Get("MainTimer");
+
+
+MainTimer.Timers.Add(() => {
+    switch (StateProp) {
+        case "Loading":
+            SetStateStart();
+            break;
+        case "Start":
+            SetStateSleep();
+            break;
+        case "Sleep":
+            SetStatePersonal();
+            break;
+        case "Personal":
+            SetStateSleep();
+            break;
+        case "End":
+            SetStateLeaderBoard();
+            break;
+        case "LeaderBoard":
+            Game.RestartGame();
+            break;
+    }
+});
+
+
+function SetStateStart() {
+    StateProp.Value = "Start";
+    MainTimer.Restart(StartTime);
+}
+
+function SetStateSleep() {
+    StateProp.Value = "Sleep";
+    RoundProp.Value++;
+    MainTimer.Restart(SleepTime);
+
+    if (RoundProp > MaxRounds_Count);
+}
+
+function SetStatePersonal() {
+    StateProp.Value = "Personal";
+    MainTimer.Restart(PersonalTime);
+}
+
+function SetStateLeaderBoard() {
+    StateProp.Value = "LeaderBoard";
+    MainTimer.Restart(LeaderBoard_Time);
+}
 
 
 
